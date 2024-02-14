60cc880c45bffcc187cf3a53f73a72ab21aa2c22
Fernando <fernando.asg@outlook.com>
2024-02-14 08:33:51 -0400

    Retrieving Data using HTTP

--- Angular-GettingStarted/APM-Start/src/app/products/product-list.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit} from "@angular/core";
+import { Component, OnDestroy, OnInit} from "@angular/core";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
     
     private _listFilter: string = '';
     get listFilter(): string {
@@ -30,8 +33,17 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+      this.sub = this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+      });
+    }
+
+    ngOnDestroy() {
+      this.sub.unsubscribe();
     }
 
     toggleImage(): void {
