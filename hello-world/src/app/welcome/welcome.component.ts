dbe9f8645128902c8c682997e2c572e00361f8c9
Fernando <fernando.asg@outlook.com>
2024-02-14 11:31:10 -0400

    Building, testing and deploying with the CLI

---
+++ hello-world/src/app/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hw-welcome',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent {
+
+}
